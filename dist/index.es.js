751ed38497cbc7031e96491d526f6dd4d867d708
Michael Daross <daross@teamshares.com>
2022-10-27 17:13:56 -0500

    Icon registration and init script

--- dist/index.es.js
+++ dist/index.es.js
@@ -18361,6 +18361,36 @@ SlAvatar = __decorateClass([
   n$1("sl-avatar")
 ], SlAvatar);
 
+const Honeybadger = require("@honeybadger-io/js");
+
+const getAppContext = () => {
+  const heroku = process.env.HEROKU_APP_NAME || "";
+  if (heroku.length === 0) return;
+  return heroku.includes("-production") ? "production" : heroku.includes("-staging") ? "staging" : "review";
+};
+
+const initHoneybadger = (opts = {}) => {
+  if (process.env.RAILS_ENV !== "production") return;
+  const appContext = getAppContext();
+
+  if (!appContext) {
+    console.log("WARN: production-ish environment is missing HEROKU_APP_NAME -- NOT initializing Honeybadger");
+    return;
+  }
+
+  if (!process.env.HONEYBADGER_JS_API_KEY) {
+    console.log(`Honeybadger not configured -- set HONEYBADGER_JS_API_KEY to enable (for ${process.env.HEROKU_APP_NAME})`);
+    return;
+  }
+
+  const config = Object.assign({
+    apiKey: process.env.HONEYBADGER_JS_API_KEY,
+    environment: appContext
+  }, opts);
+  Honeybadger.configure(config);
+  window.Honeybadger = Honeybadger;
+};
+
 /*
 Stimulus 3.1.0
 Copyright © 2022 Basecamp, LLC
@@ -18824,40 +18854,19 @@ class _class extends Controller {
 
 _defineProperty(_class, "targets", ["switchable", "clickable"]);
 
-const Honeybadger = require("@honeybadger-io/js");
+/** ***************************************** */
 
-const getAppContext = () => {
-  const heroku = process.env.HEROKU_APP_NAME || "";
-  if (heroku.length === 0) return;
-  return heroku.includes("-production") ? "production" : heroku.includes("-staging") ? "staging" : "review";
-};
+/**  Apps should call this method on startup  */
 
-const initHoneybadger = (opts = {}) => {
-  if (process.env.RAILS_ENV !== "production") return;
-  const appContext = getAppContext();
+/** ***************************************** */
 
-  if (!appContext) {
-    console.log("WARN: production-ish environment is missing HEROKU_APP_NAME -- NOT initializing Honeybadger");
-    return;
-  }
-
-  if (!process.env.HONEYBADGER_JS_API_KEY) {
-    console.log(`Honeybadger not configured -- set HONEYBADGER_JS_API_KEY to enable (for ${process.env.HEROKU_APP_NAME})`);
-    return;
-  }
-
-  const config = Object.assign({
-    apiKey: process.env.HONEYBADGER_JS_API_KEY,
-    environment: appContext
-  }, opts);
-  Honeybadger.configure(config);
-  window.Honeybadger = Honeybadger;
-};
-
-const registerIcons = () => {
+const initialize = () => {
+  initHoneybadger({
+    debug: true
+  });
   registerIconLibrary("default", {
     resolver: name => `https://cdn.jsdelivr.net/npm/heroicons@2.0.1/24/outline/${name}.svg`
   });
 };
 
-export { input_clipboard_controller as InputClipboardController, input_mask_controller as InputMaskController, _class as SwitchController, _class$1 as ToggleController, initHoneybadger, registerIcons };
+export { input_clipboard_controller as InputClipboardController, input_mask_controller as InputMaskController, _class as SwitchController, _class$1 as ToggleController, initHoneybadger, initialize };
--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 import "./components";
+import { registerIconLibrary } from "@shoelace-style/shoelace";
+import { initHoneybadger } from "./_honeybadger";
 
 export * from "./controllers";
-export * from "./_honeybadger";
-export * from "./icons";
+export * from "./_honeybadger"; // Leaving this for legacy sake; should be removed once everyone is calling initialize below
+
+/** ***************************************** */
+/**  Apps should call this method on startup  */
+/** ***************************************** */
+export const initialize = () => {
+  initHoneybadger({ debug: true });
+
+  registerIconLibrary("default", {
+    resolver: name => `https://cdn.jsdelivr.net/npm/heroicons@2.0.1/24/outline/${name}.svg`
+  });
+};
