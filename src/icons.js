85015d74d0fa5f48cc5451522082475200818079
Michael Daross <github@crookedgrin.com>
2022-10-27 17:18:51 -0500

    Delete icons.js

--- src/icons.js
+++
@@ -1,7 +0,0 @@
-import { registerIconLibrary } from "@shoelace-style/shoelace/dist/utilities/icon-library.js";
-
-export const registerIcons = () => {
-  registerIconLibrary("default", {
-    resolver: name => `https://cdn.jsdelivr.net/npm/heroicons@2.0.1/24/outline/${name}.svg`
-  });
-};
