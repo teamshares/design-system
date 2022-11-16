ef96fb535aa63992a82a2047a47ffe465f147f2f
Kali Donovan <kali@teamshares.com>
2022-11-16 15:12:26 -0800

    Adding a test bin script (unlikely hope it'll symlink correctly if it's pulled directly from remote, not locally linked)

---
+++ bin/ts-postcss-build-script.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+
+// NOTE: couldn't get this picked up in local development...
+//
+// TODO: Kali: leaving in place for now to see if it's a local linking issue...
+// either replace each app's postcss.config.js + esbuild.config.js with shared scripts (if it starts working),
+// else remove this script entirely.
+console.log("Hi Kali");
