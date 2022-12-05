985ffab2282bc3f6104dabe7b27346814a43ca76
Kali Donovan <kali@teamshares.com>
2022-12-05 12:14:56 -0800

    Move InputClipboardController back to buyout

--- js/controllers/index.js
+++ js/controllers/index.js
@@ -1,6 +1,5 @@
-import InputClipboardController from "./input_clipboard_controller";
 import InputMaskController from "./input_mask_controller";
 import ToggleController from "./toggle_controller";
 import SwitchController from "./switch_controller";
 
-export { InputClipboardController, InputMaskController, ToggleController, SwitchController };
+export { InputMaskController, ToggleController, SwitchController };
