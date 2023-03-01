1ee57dbd87f9ac841ce030cfedc98f4c9a1b37bd
Michael Daross <daross@teamshares.com>
2023-03-01 10:01:40 -0600

    Importing Rails in shared-ui and adding shoelace selectors (#131)

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 import * as Shoelace from "@teamshares/shoelace"; // eslint-disable-line no-unused-vars
 import { initHoneybadger } from "./_honeybadger";
+import Rails from "@rails/ujs";
 
 export * from "./controllers";
 export * from "./_honeybadger"; // Leaving this for legacy sake; should be removed once everyone is calling initialize below
@@ -11,5 +12,14 @@ export default class Teamshares {
   static init () {
     console.log("Initializing Teamshares JS");
     initHoneybadger({ debug: true });
+
+    // Overwrite Rails UJS's selectors to include Shoelace elements
+    Rails.buttonClickSelector = {
+      selector: "button[data-remote]:not([form]), button[data-confirm]:not([form]), sl-button[data-remote]:not([form]), sl-button[data-confirm]:not([form])",
+      exclude: "form button, form sl-button",
+    };
+    Rails.linkClickSelector = "a[data-confirm], a[data-method], a[data-remote]:not([disabled]), a[data-disable-with], a[data-disable], sl-button[href][data-confirm], sl-button[href][data-method], sl-button[href][data-remote]:not([disabled]), sl-button[href][data-disable-with], sl-button[href][data-disable]";
+
+    Rails.start();
   }
 }
