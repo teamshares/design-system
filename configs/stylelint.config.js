0ebb1a4559f9906a5172198cb4fb69e84850a1e4
Michael Daross <daross@teamshares.com>
2023-03-24 12:28:25 -0500

    Update stylelint to allow unit-less CSS variables (#143)
    * Update stylelint.config.js
    
    * Update stylelint.config.js

--- configs/stylelint.config.js
+++ configs/stylelint.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   extends: "stylelint-config-sass-guidelines",
   rules: {
+    // Without this exception, stylelint will incorrectly append "px" to any "0" value, which can break calc() functions
+    "length-zero-no-unit": [true, {
+      ignore: ["custom-properties"],
+    }],
     "max-nesting-depth": 4,
     "selector-max-compound-selectors": 4,
     "selector-max-id": 1,
