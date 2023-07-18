15987941606d6ecd8c3e997b4d752805bc2a7aa6
Michael Daross <daross@teamshares.com>
2023-07-18 12:27:30 -0500

    Mad/rails as param (#179)
    * Passing in Rails as a parameter to init
    
    * Updating rails to 7.0.6 to match OS
    
    * Add return value for Rails import
    
    * Updating comments
    
    * Removing return option

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 import * as Shoelace from "@teamshares/shoelace";
 import { initHoneybadger } from "./_honeybadger";
+// Importing this as a default param so the init doesn't break if apps don't pass it in
 import Rails from "@rails/ujs";
 
 export * from "./controllers";
@@ -9,23 +10,23 @@ export * from "./_honeybadger"; // Leaving this for legacy sake; should be remov
 /**  Apps should import Teamshares and call init() on startup  */
 /** ********************************************************** */
 export default class Teamshares {
-  static init () {
+  static init (railsObj = Rails) {
     console.log("Initializing Teamshares JS");
     initHoneybadger({ debug: true });
 
     // Overwrite Rails UJS's selectors to include Shoelace elements
-    Rails.buttonClickSelector = {
-      selector: Rails.buttonClickSelector.selector + ", sl-button[data-remote]:not([form]), sl-button[data-confirm]:not([form])",
+    railsObj.buttonClickSelector = {
+      selector: railsObj.buttonClickSelector.selector + ", sl-button[data-remote]:not([form]), sl-button[data-confirm]:not([form])",
       exclude: "form button, form sl-button",
     };
-    Rails.linkClickSelector += ", sl-button[href][data-confirm], sl-button[href][data-method], sl-button[href][data-remote]:not([disabled]), sl-button[href][data-disable-with], sl-button[href][data-disable]";
-    Rails.inputChangeSelector += ", sl-select[data-remote], sl-input[data-remote], sl-textarea[data-remote]";
-    Rails.formInputClickSelector += ", form:not([data-turbo=true]) sl-input[type=submit], form:not([data-turbo=true]) sl-input[type=image], form:not([data-turbo=true]) sl-button[type=submit], form:not([data-turbo=true]) sl-button:not([type]), sl-input[type=submit][form], sl-input[type=image][form], sl-button[type=submit][form], sl-button[form]:not([type])";
-    Rails.formDisableSelector += ", sl-input[data-disable-with]:enabled, sl-button[data-disable-with]:enabled, sl-textarea[data-disable-with]:enabled, sl-input[data-disable]:enabled, sl-button[data-disable]:enabled, sl-textarea[data-disable]:enabled";
-    Rails.formEnableSelector += ", sl-input[data-disable-with]:disabled, sl-button[data-disable-with]:disabled, sl-textarea[data-disable-with]:disabled, sl-input[data-disable]:disabled, sl-button[data-disable]:disabled, sl-textarea[data-disable]:disabled";
-    Rails.fileInputSelector += ", sl-input[name][type=file]:not([disabled])";
-    Rails.linkDisableSelector += ", sl-button[href][data-disable-with], sl-button[href][data-disable]";
-    Rails.buttonDisableSelector += ", sl-button[data-remote][data-disable-with], sl-button[data-remote][data-disable]";
+    railsObj.linkClickSelector += ", sl-button[href][data-confirm], sl-button[href][data-method], sl-button[href][data-remote]:not([disabled]), sl-button[href][data-disable-with], sl-button[href][data-disable]";
+    railsObj.inputChangeSelector += ", sl-select[data-remote], sl-input[data-remote], sl-textarea[data-remote]";
+    railsObj.formInputClickSelector += ", form:not([data-turbo=true]) sl-input[type=submit], form:not([data-turbo=true]) sl-input[type=image], form:not([data-turbo=true]) sl-button[type=submit], form:not([data-turbo=true]) sl-button:not([type]), sl-input[type=submit][form], sl-input[type=image][form], sl-button[type=submit][form], sl-button[form]:not([type])";
+    railsObj.formDisableSelector += ", sl-input[data-disable-with]:enabled, sl-button[data-disable-with]:enabled, sl-textarea[data-disable-with]:enabled, sl-input[data-disable]:enabled, sl-button[data-disable]:enabled, sl-textarea[data-disable]:enabled";
+    railsObj.formEnableSelector += ", sl-input[data-disable-with]:disabled, sl-button[data-disable-with]:disabled, sl-textarea[data-disable-with]:disabled, sl-input[data-disable]:disabled, sl-button[data-disable]:disabled, sl-textarea[data-disable]:disabled";
+    railsObj.fileInputSelector += ", sl-input[name][type=file]:not([disabled])";
+    railsObj.linkDisableSelector += ", sl-button[href][data-disable-with], sl-button[href][data-disable]";
+    railsObj.buttonDisableSelector += ", sl-button[data-remote][data-disable-with], sl-button[data-remote][data-disable]";
 
     // Register free font-awesome icons
     Shoelace.registerIconLibrary("fa-free", {
