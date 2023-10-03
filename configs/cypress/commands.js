94d8459db604e496605b8fe41846a4e414b0f9e6
Kali Donovan <kali@teamshares.com>
2023-10-03 16:41:18 -0700

    Extracting shared cypress commands

---
+++ configs/cypress/commands.js
@@ -0,0 +1,27 @@
+// These commands are available to all cypress specs
+// (they reference paths added via teamshares-rails)
+
+Cypress.Commands.add("factory", (name, opts = {}) => {
+  const { traits, ...attributes } = opts;
+  cy.request("POST", "teamshares/cypress/factories", {
+    name,
+    traits,
+    attributes,
+  });
+});
+
+Cypress.Commands.add("factoryId", (alias, callbackFn) => {
+  cy.get(alias).then((factoryResponse) => { callbackFn(factoryResponse.body.id); });
+});
+
+Cypress.Commands.add("login", (email) => {
+  cy.request("POST", "teamshares/cypress/sessions", { email });
+});
+
+Cypress.Commands.add("flipper_enable", (feature) => {
+  cy.request("POST", "teamshares/cypress/flippers", { feature });
+});
+
+Cypress.Commands.add("flipper_disable", (feature) => {
+  cy.request("DELETE", "teamshares/cypress/flippers", { feature });
+});
