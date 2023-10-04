4d99b045681e5510b5f514acf82948d36fd48059
Kali Donovan <kali@teamshares.com>
2023-10-03 17:06:56 -0700

    Shared JS: extracting stimulus

--- rails-js/controllers/index.js
+++ rails-js/controllers/index.js
@@ -1,4 +1,23 @@
-import InputMaskController from "./input_mask_controller";
+import { Application } from "@hotwired/stimulus";
+
+// Shared controllers
 import ToggleController from "./toggle_controller";
 
-export { InputMaskController, ToggleController };
+const registerStimulusControllers = (appDefinitions) => {
+  const application = Application.start();
+
+  // Register the shared controllers
+  application.register("toggle", ToggleController);
+
+  // Auto-load all app controllers
+  application.load(appDefinitions);
+
+  // Configure Stimulus development experience
+  application.warnings = true;
+  application.debug = false;
+  window.Stimulus = application;
+
+  return application;
+};
+
+export { registerStimulusControllers };
