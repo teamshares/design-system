9e6e15b334b480b1a555d6e647541c50ddd143a5
Kali Donovan <kali@teamshares.com>
2023-12-13 14:14:23 -0800

    Tried (and then reverted) updating deprecated postcss plugin syntax

--- configs/postcss/prefix-component-classes.js
+++ configs/postcss/prefix-component-classes.js
@@ -9,10 +9,36 @@ const prefixComponentClasses = postcss.plugin('prefix-component-classes', () => 
       const identifier = matches[1].replaceAll("/", "--").replaceAll("_", "-");
 
       root.walkRules(rule => {
-        console.log(`[${identifier}] ${rule.selector}`);
+        // console.log(`[${identifier}] ${rule.selector}`);
         rule.selector = `.c-${identifier} ${rule.selector}`;
       });
     };
 });
+
+// NOTE: in *theory* the code below should switch to above plugin from v7 to v8...
+// See migrations guide: https://evilmartians.com/chronicles/postcss-8-plugin-migration.
+
+// Unfortunately, it starts throwing "CssSyntaxError: postcss-easy-import: <file>: Unknown word"
+// errors for // comments
+
+// const prefixComponentClasses = () => {
+//     return {
+//       postcssPlugin: "prefix-component-classes",
+//       Once (root, result) {
+//         const matches = result.opts.from.match(/frontend\/components\/?(.*)\/[^/]+?.s?css$/);
+
+//         // Do not transform CSS files from outside of the components folder
+//         if (!matches) return;
+
+//         const identifier = matches[1].replaceAll("/", "--").replaceAll("_", "-");
+
+//         root.walkRules(rule => {
+//           console.log(`[${identifier}] ${rule.selector}`);
+//           rule.selector = `.c-${identifier} ${rule.selector}`;
+//         });
+//       },
+//     };
+// }
+// prefixComponentClasses.postcss = true;
 
 module.exports = prefixComponentClasses
