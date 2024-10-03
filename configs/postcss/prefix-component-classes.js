e001712b406d2e911fa6325b3d4a256abc76c18e
Kali Donovan <kali@teamshares.com>
2024-10-03 16:49:55 -0700

    General cleanup

--- configs/postcss/prefix-component-classes.js
+++ configs/postcss/prefix-component-classes.js
@@ -2,6 +2,26 @@ var clc = require("cli-color");
 const DEBUG = process.env.LOG_CSS === "debug";
 const INFO = DEBUG || process.env.LOG_CSS === "info";
 
+const parseRule = (wrapper) => (rule) => {
+  if (rule.selector == "._base") {
+    // If the selector is our special `._base` rule, apply those styles directly to the generated class itself
+    if (DEBUG) console.log(`\tReplacing ${clc.whiteBright(rule.selector)} with ${clc.whiteBright(wrapper)}`);
+    rule.selector = wrapper;
+  } else if (rule.selector.startsWith("._base")) {
+    // We do not support nesting anything under ._base (just make it a top-level rule and it'll be auto-nested)
+    console.log(clc.red("\tDROPPING SELECTOR:", clc.redBright(rule.selector), clc.red("(do not nest anything under ._base)")));
+    rule.remove();
+  } else if (rule.selector.startsWith("._component")) {
+    // DEPRECATED: ideally we'd remove all ._component selectors, but for now we'll just strip the wrapper
+    const new_selector = `${wrapper}${rule.selector.replace("._component", "")}`;
+    if (INFO) console.log(clc.yellow(`\tRemoving old-style ._component wrapper from ${clc.yellowBright(rule.selector)}`));
+    rule.selector = new_selector;
+  } else {
+    // Default case: prefix the selector with the component class
+    if (DEBUG) console.log(clc.white(`\tPrefixing ${clc.whiteBright(rule.selector)}`));
+    rule.selector = `${wrapper} ${rule.selector}`;
+  }
+};
 
 const prefixComponentClasses = () => {
   return {
@@ -14,28 +34,10 @@ const prefixComponentClasses = () => {
 
       const filePath = matches[0].replace("frontend/components/", "");
       const identifier = matches[1].replaceAll("/", "--").replaceAll("_", "-");
-      const info = INFO && identifier === "sidecar-styles-demo"
-      const debug = DEBUG && identifier === "sidecar-styles-demo"
-      if (info) console.log("================= Prefixing component classes for", clc.cyan(filePath));
+      const wrapper = `.c-${identifier}`;
 
-      root.walkRules(rule => {
-        // If the selector is our special `._base` rule, apply those styles directly to the generated class itself
-        if (rule.selector == "._base") {
-          if (debug) console.log(`\tReplacing ${rule.selector} with .c-${identifier}`);
-          rule.selector = `.c-${identifier}`;
-        } else if (rule.selector.startsWith("._base")) {
-          console.log(clc.red("\tDROPPING SELECTOR:", clc.redBright(rule.selector), clc.red("(do not nest anything under ._base)")));
-          rule.remove();
-        } else if (rule.selector.startsWith("._component")) {
-          if (info) console.log(rule.selector)
-          const new_selector = `.c-${identifier}${rule.selector.replace("._component", "")}`;
-          if (info) console.log(clc.yellow("\tRemoving old-style '._component' wrapper:"), `replacing ${rule.selector} with ${new_selector}`);
-          rule.selector = new_selector;
-        } else {
-          if (debug) console.log(`\tPrefixing ${rule.selector} with .c-${identifier}`);
-          rule.selector = `.c-${identifier} ${rule.selector}`;
-        }
-      });
+      if (INFO) console.log("================= Prefixing selectors in component", clc.cyanBright(filePath), "with", clc.cyan(wrapper));
+      root.walkRules(parseRule(wrapper));
     },
   };
 }
